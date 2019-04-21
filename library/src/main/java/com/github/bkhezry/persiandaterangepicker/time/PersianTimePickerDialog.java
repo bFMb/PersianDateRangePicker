e599720a344a1d94c9df5ee6e0cbf39599e0cc77
bFMb <dard.cool@gmail.com>
2019-04-21 11:07:40 +0430

    Update import DialogFragment

--- library/src/main/java/com/github/bkhezry/persiandaterangepicker/time/PersianTimePickerDialog.java
+++ library/src/main/java/com/github/bkhezry/persiandaterangepicker/time/PersianTimePickerDialog.java
@@ -18,7 +18,7 @@ package com.github.bkhezry.persiandaterangepicker.time;
 
 import android.animation.ObjectAnimator;
 import android.app.ActionBar.LayoutParams;
-import android.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
